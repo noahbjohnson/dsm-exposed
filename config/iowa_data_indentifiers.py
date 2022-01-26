b46e0b4e2f54532fe8c910cccf57ce8fa2e15aad
noahbjohnson <njohnson@cloudtamer.io>
2022-01-26 17:10:05 -0600

    add dataset ids

---
+++ config/iowa_data_indentifiers.py
@@ -0,0 +1,8 @@
+source_domain = "data.iowa.gov"
+
+#  Iowa Campaign Contributions Received
+# https://dev.socrata.com/foundry/data.iowa.gov/smfg-ds7h
+
+campaign_contributions_dataset_id = "smfg-ds7h"
+
+registered_political_entities = "5dtu-swbk"
